e7f01717bd78eccc40035bb4b44f93a20006820b
Nicholas <kolyaganin@outlook.com>
2020-06-30 21:48:18 +0300

    Добавил интерактивную карту

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
-var feedbackOverlay = document.querySelector(".modal-overlay");
-var feedbackPopup = feedbackOverlay.querySelector(".feedback-modal-group");
-var feedbackLink = document.querySelector(".feedback-link");
-var feedbackClose = document.querySelector(".feedback-close-button");
-var form = feedbackOverlay.querySelector("form");
-var nameField = feedbackOverlay.querySelector("[name=user-id]");
-var emailField = feedbackOverlay.querySelector("[name=email");
-var feedbackField = feedbackOverlay.querySelector("[name=feedback]");
+let feedbackOverlay = document.querySelector(".modal-overlay");
+let feedbackPopup = feedbackOverlay.querySelector(".feedback-modal-group");
+let feedbackLink = document.querySelector(".feedback-link");
+let feedbackClose = document.querySelector(".feedback-close-button");
+let form = feedbackOverlay.querySelector("form");
+let nameField = feedbackOverlay.querySelector("[name=user-id]");
+let emailField = feedbackOverlay.querySelector("[name=email");
+let feedbackField = feedbackOverlay.querySelector("[name=feedback]");
 
-var isStorageSupport = true;
-var storageName = "";
-var storageEmail = "";
+let isStorageSupport = true;
+let storageName = "";
+let storageEmail = "";
+
+const popupClose = () => {
+  setTimeout(function() {
+    feedbackOverlay.classList.remove("modal-show");
+    feedbackPopup.classList.remove("feedback-close");
+  }, 600);
+}
 
 try {
   storageName = localStorage.getItem("name");
@@ -21,7 +28,6 @@ try {
 feedbackLink.addEventListener("click", function(evt) {
   evt.preventDefault();
   feedbackOverlay.classList.add("modal-show");
-  feedbackPopup.classList.remove("feedback-close");
   if (storageName || storageEmail) {
     nameField.value = storageName;
     emailField.value = storageEmail;
@@ -39,17 +45,16 @@ feedbackClose.addEventListener("click", function(evt) {
   evt.preventDefault();
   feedbackPopup.classList.remove("feedback-err");
   feedbackPopup.classList.add("feedback-close");
-  setTimeout(function() {
-    feedbackOverlay.classList.remove("modal-show");
-  }, 600);
+  popupClose();
 });
 
 window.addEventListener("keydown", function(evt) {
   if (evt.keyCode === 27) {
     if (feedbackOverlay.classList.contains("modal-show")) {
       evt.preventDefault();
-      feedbackOverlay.classList.remove("modal-show");
       feedbackPopup.classList.remove("feedback-err");
+      feedbackPopup.classList.add("feedback-close");
+      popupClose();
     }
   }
 });
@@ -66,5 +71,4 @@ form.addEventListener("submit", function(evt) {
       localStorage.setItem("email", emailField.value);
     }
   }
-
 });
